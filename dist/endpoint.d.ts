b2daeac1e43155eead30dc5aa55ebee8699575c7
andy <andy.d@oolio.com>
2023-04-25 12:31:31 +0700

    feat: update workflow release

--- dist/endpoint.d.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from 'express';
-import { Registry } from 'prom-client';
-export interface EndpointOptions {
-    app: Express;
-    path?: string;
-    register?: Registry;
-}
-export declare function registerEndpoint({ register, app, path }: EndpointOptions): void;
--- dist/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-import { LabelValues } from 'prom-client';
-export declare function filterLabels(labels: LabelValues<string>): LabelValues<string>;
-export declare function convertMsToS(ms: number): number;
